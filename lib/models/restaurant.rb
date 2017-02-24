b51523fc2e1a62a370b45ce910173b1e824598a4
Raoul DIFFOUO <diraulo@gmail.com>
2017-02-24 13:23:19 +0200

    add some properties restaurant model and some specs

--- lib/models/restaurant.rb
+++ lib/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant
   include DataMapper::Resource
 
-  property :id, Serial, key: true
-  property :name, String, length: 128
-
+  property :id,          Serial, key: true
+  property :name,        String, length: 128
+  property :description, Text
+  property :address,     String, length: 128
 end
